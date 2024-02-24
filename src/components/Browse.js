4d31a921a19d05adda2e0cc0afc149fd4784408e
jatingoel7880 <jating.0311@gmail.com>
2024-02-24 16:59:02 +0530

    "add"

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,26 +1,26 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Header from "./Header";
-import { API_OPTIONS } from "../utils/constants";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 
 const Browse = () => {
-  const getNowPlayingMovies = async () => {
-    const data= await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?page=1", API_OPTIONS);
-     const json= await data.json();
-     console.log(json);
-      // .then((response) => response.json())
-      // .then((response) => console.log(response))
-      // .catch((err) => console.error(err));
-  };
+  useNowPlayingMovies();
 
-  useEffect(()=>{
-    getNowPlayingMovies();
-  },[])
-
-  
   return (
     <div>
       <Header />
+      <MainContainer />
+      <SecondaryContainer />
+
+      {/* 
+      Main Container 
+        -VideoBackground
+        -VideoTitle
+      Secondary Conatiner
+        -MovieList * n
+        -cards * n
+      */}
     </div>
   );
 };
